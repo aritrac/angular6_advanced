d5df7f373a836df67798c878b5b88dea8897ac0f
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-09-30 19:41:17 +0530

    Adding practice exercises

---
+++ exercise-practice-databinding/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'exercise-practice-databinding';
+
+  username = "Enter name here";
+}
