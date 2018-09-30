6955a7ab91d54f29e1ac4e6f287a34641c094811
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-09-30 20:13:23 +0530

    Added complete exercise examples

--- exercise-practice-databinding/src/app/app.component.ts
+++ exercise-practice-databinding/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'exercise-practice-databinding';
+  isClickable = false;
 
-  username = "Enter name here";
+  userName = "";
+
+  onUpdateUserName(event: any){
+    this.userName = (<HTMLInputElement>event.target).value;
+    if(this.userName.length > 0){
+      this.isClickable = true;
+    }else{
+      this.isClickable = false;
+    }
+  }
+
+  onClickEmptyUserName(){
+    this.userName = "";
+    this.isClickable = false;
+  }
 }
