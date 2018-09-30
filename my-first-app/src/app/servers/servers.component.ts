0af981738558dcea84e8200f6b6d514538af1b9c
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-09-30 21:09:41 +0530

    Added ngFor directive

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //we can have either element selectors as below or we can have attribute selectors. Both the flavours are shown below
-  selector: 'app-servers',        //element selector
-  //selector: '[app-servers]',    //attribute selector
-  //selector: '.app-servers',     //class selector
+  // we can have either element selectors as below or we can have attribute selectors. Both the flavours are shown below
+  selector: 'app-servers',         // element selector
+  // selector: '[app-servers]',    // attribute selector
+  // selector: '.app-servers',     // class selector
   templateUrl: './servers.component.html',
   // template: `<app-server></app-server>
   //            <app-server></app-server>`,
@@ -15,8 +15,9 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
-  constructor() { 
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
@@ -25,12 +26,13 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
-  onCreateServer(){
+  onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
-  onUpdateServerName(event: any){
+  onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 
