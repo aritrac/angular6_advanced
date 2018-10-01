c2dc3c08daf3315e5fdcc733fd3b55d64e0196b9
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-10-01 09:06:17 +0530

    Added directives exercise

--- exercise-practice-directives/src/app/app.component.ts
+++ exercise-practice-directives/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercise-practice-directives';
+  isParaVisible = false;
+  clickDetails = [];
+
+  onClickTogglePara() {
+    // The toggling logic for the para visibility
+    this.isParaVisible = !this.isParaVisible;
+
+    // storing the click number in the clickDetails array
+    // so the ngFor can iterate over it to display the click-details-component n times
+    this.clickDetails.push(this.clickDetails.length + 1);
+  }
 }
