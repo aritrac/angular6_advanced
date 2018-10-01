9e2992049991229940f174bdebafb2d093f7923b
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-10-01 11:57:49 +0530

    Added styling to components and little functionality

--- shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Meat Recipe', 'This is made of meat', 'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
+    new Recipe('Fish Recipe', 'This is made of fish', 'https://c.pxhere.com/photos/50/03/beirut_power_supply_snack-1375814.jpg!d')
+  ];
 
   constructor() { }
 
