50963bc819a350bf11c6ceeb6c5eb1d576e372b6
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-10-01 16:44:12 +0530

    Adding event emit and property binding

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = ''; commented this out as newServerName is being fetched from the HTMLInputElement reference using local references
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+  }
+
+}
